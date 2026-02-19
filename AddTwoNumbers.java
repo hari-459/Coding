463b4983aebe024d502748342d07fb45482ba5b7
hari-459 <harimadhavan459@gmail.com>
2026-02-19 21:46:06 +0530

    Implement AddTwoNumbers class to sum two linked lists

---
+++ AddTwoNumbers.java
@@ -0,0 +1,28 @@
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class AddTwoNumbers{
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res=new ListNode(0);
+        ListNode temp=res;
+        int carry=0;
+        while(l1!=null || l2!=null || carry!=0)
+        {
+            int x=0,y=0;
+            if(l1!=null) x=l1.val;
+            if(l2!=null) y=l2.val;
+            int sum=x+y+carry;
+            carry=sum/10;
+            int data=sum%10;
+            temp.next=new ListNode(data);
+            temp=temp.next;
+            if(l1!=null)l1=l1.next;
+            if(l2!=null)l2=l2.next;
+        }
+        return res.next;
+    }
+}
