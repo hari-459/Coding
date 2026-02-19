c51f21ba8538833a42b2acbc85fe60ae2f802de1
hari-459 <harimadhavan459@gmail.com>
2026-02-19 21:49:19 +0530

    Build an Array With Stack Operations

---
+++ BuildArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> res=new ArrayList<>();
+        int i=1;
+        for(int num:target)
+        {
+            while(i<num)
+            {
+                res.add("Push");
+                res.add("Pop");
+                i++;
+            }
+            res.add("Push");
+            i++;
+        }
+        return res;
+    }
+}
