bc0010ec068fa566a6fb02cd57e9407d4ad5f1ba
hari-459 <harimadhavan459@gmail.com>
2026-02-19 21:38:46 +0530

    Implement solution to find error numbers in array

---
+++ SetMismatch.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n=nums.length;
+        int[] t=new int[n+1];
+        int[] res=new int[2];
+        for(int i=0;i<n;i++)
+        {
+            t[nums[i]]++;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(t[i]>1) res[0]=i;
+            if(t[i]==0) res[1]=i;
+        }
+        return res;
+    }
+}
