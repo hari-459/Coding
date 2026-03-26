608af6709d3bcd94be7940c4ddc43a3bf7d85a87
Hari Madhavan S <harimadhavan459@gmail.com>
2026-03-26 22:38:05 +0530

    0231-power-of-two N/A (0%) N/A (0%) .java

--- LeetCode/Easy/0231-power-of-two/0231-power-of-two.java
+++ LeetCode/Easy/0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        if(n==1) return true;
-        while(n != 1)
-        {
-            if(n%2 == 0) n/=2;
-            else return false;
-        }
-        return true;
+        return (n > 0 && (n & n-1) == 0);
     }
 }
