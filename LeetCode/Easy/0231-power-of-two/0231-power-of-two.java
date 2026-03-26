cbda566b826646313e88cc054bea1b61754485d6
Hari Madhavan S <harimadhavan459@gmail.com>
2026-03-26 22:26:23 +0530

    0231-power-of-two 1 ms (96.39%) 42.6 MB (49.07%) .java

---
+++ LeetCode/Easy/0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        while(n != 1)
+        {
+            if(n%2 == 0) n/=2;
+            else return false;
+        }
+        return true;
+    }
+}
